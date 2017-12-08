24bb2f779af152912dce24280c3c1259b2cef74f
Haibin Lee <haibin.li@newtouch.cn>
2017-12-08 19:53:48 +0800

    REV: set email max length to 50

--- apps/users/models/user.py
+++ apps/users/models/user.py
@@ -30,7 +30,7 @@ class User(AbstractUser):
 
     username = models.CharField(max_length=20, unique=True, verbose_name=_('Username'))
     name = models.CharField(max_length=20, verbose_name=_('Name'))
-    email = models.EmailField(max_length=30, unique=True, verbose_name=_('Email'))
+    email = models.EmailField(max_length=50, unique=True, verbose_name=_('Email'))
     groups = models.ManyToManyField(UserGroup, related_name='users', blank=True, verbose_name=_('User group'))
     role = models.CharField(choices=ROLE_CHOICES, default='User', max_length=10, blank=True, verbose_name=_('Role'))
     avatar = models.ImageField(upload_to="avatar", null=True, verbose_name=_('Avatar'))
